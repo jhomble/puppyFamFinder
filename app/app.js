44821bace5ca03d1656bcfa2cac76713f6778266
jhomble <jhomble7@gmail.com>
2017-06-07 00:37:33 -0400

    make it better

--- app/app.js
+++ app/app.js
@@ -15,23 +15,57 @@ myApp.controller('home', ['$scope', '$firebaseArray', function ($scope, $firebas
 
   $scope.database = $firebaseArray(dbRef.child('family'))
   $scope.database.$loaded().then(function () {
-    $scope.database.forEach(function(dog) {
+    $scope.database.forEach(function (dog) {
       console.log(dog)
       var ref = storageRef.child('pets/' + dog.picture)
       console.log(ref)
-      ref.getDownloadURL().then(function(url){
+      ref.getDownloadURL().then(function (url) {
         dog.image = url
         $scope.$apply()
       })
     });
   })
 
+  $scope.petName = ""
+  $scope.ownerName = ""
+  $scope.address = ""
+  $scope.breed = ""
+  $scope.bio = ""
+  $scope.contact = ""
+
+  var formCheck = function () {
+    if ($scope.petName.length() == 0 || $scope.ownerName.length() == 0 ||
+      $scope.address.length() == 0 || $scope.breed.length() == 0 ||
+      $scope.bio.length() == 0 || $scope.contact.length() == 0) {
+      console.log("error")
+      return false
+    }
+    return true
+  }
 
   $scope.addToDB = function () {
-    var family = $firebaseArray(dbRef.child('family'))
-    family.$add({
-      name: 'fudge'
-    })
+    
+    if(formCheck){
+      
+      var family = $firebaseArray(dbRef.child('family'))
+      family.$add({
+        address: $scope.address,
+        bio: $scope.bio,
+        breed: $scope.breed,
+        contact: $scope.contact,
+        name: $scope.petName,
+        owner: $scope.ownerName
+      }).then(function(){
+        console.log("done")
+        $scope.petName = ""
+        $scope.ownerName = ""
+        $scope.address = ""
+        $scope.breed = ""
+        $scope.bio = ""
+        $scope.contact = ""
+        $scope.$apply()
+      })
+    }
   }
 }]);
 

48b0bb3623232fb3f47eaea0bae4a65565cfd645
jhomble <jhomble7@gmail.com>
2017-06-07 16:46:55 -0400

    form working

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ var myApp = angular.module('myApp', [
   'myApp.version'
 ]);
 
-myApp.controller('home', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+myApp.controller('home', ['$scope', '$firebaseArray', '$firebaseObject', function ($scope, $firebaseArray, $firebaseObject) {
 
   var dbRef = firebase.database().ref();
   var storageRef = firebase.storage().ref();
@@ -16,9 +16,9 @@ myApp.controller('home', ['$scope', '$firebaseArray', function ($scope, $firebas
   $scope.database = $firebaseArray(dbRef.child('family'))
   $scope.database.$loaded().then(function () {
     $scope.database.forEach(function (dog) {
-      console.log(dog)
+      // console.log(dog)
       var ref = storageRef.child('pets/' + dog.picture)
-      console.log(ref)
+      // console.log(ref)
       ref.getDownloadURL().then(function (url) {
         dog.image = url
         $scope.$apply()
@@ -32,21 +32,39 @@ myApp.controller('home', ['$scope', '$firebaseArray', function ($scope, $firebas
   $scope.breed = ""
   $scope.bio = ""
   $scope.contact = ""
+  $scope.picturePath = ""
 
   var formCheck = function () {
-    if ($scope.petName.length() == 0 || $scope.ownerName.length() == 0 ||
-      $scope.address.length() == 0 || $scope.breed.length() == 0 ||
-      $scope.bio.length() == 0 || $scope.contact.length() == 0) {
-      console.log("error")
+    if ($scope.petName === "" || $scope.ownerName === "" ||
+      $scope.address === "" || $scope.breed === "" ||
+      $scope.bio === "" || $scope.contact === "") {
+      alert("Invalid Form")
       return false
     }
     return true
   }
 
+  $scope.addPicture = function (element) {
+    console.log("23123")
+    $scope.uploadPicture = element.files[0]
+    // var fileReader = new FileReader()
+    // console.log(file)
+    // fileReader.onloadend = function (e) {
+    //   console.log("here")
+    //   var arrayBuffer = e.target.result;
+    //   var fileType = "image/jpeg"
+    //   console.log("asdsd")
+    //   blobUtil.arrayBufferToBlob(arrayBuffer, fileType).then(function (blob) {
+    //     $scope.uploadPicture = blob
+    //     console.log(uploadPicture)
+    //   }).catch(
+    //     alert("Error Uploading Image")
+    //     );
+    // }
+  }
+
   $scope.addToDB = function () {
-    
-    if(formCheck){
-      
+    if (formCheck()) {
       var family = $firebaseArray(dbRef.child('family'))
       family.$add({
         address: $scope.address,
@@ -55,15 +73,22 @@ myApp.controller('home', ['$scope', '$firebaseArray', function ($scope, $firebas
         contact: $scope.contact,
         name: $scope.petName,
         owner: $scope.ownerName
-      }).then(function(){
-        console.log("done")
-        $scope.petName = ""
-        $scope.ownerName = ""
-        $scope.address = ""
-        $scope.breed = ""
-        $scope.bio = ""
-        $scope.contact = ""
-        $scope.$apply()
+      }).then(function (result) {
+        console.log(result.path.o[1])
+        var imageRef = storageRef.child('pets/' + result.path.o[1])
+        imageRef.put($scope.uploadPicture).then(function () {
+          dbRef.child('family').child(result.path.o[1]).update({picture: result.path.o[1]})
+        }).then(function () {
+          $scope.petName = ""
+          $scope.ownerName = ""
+          $scope.address = ""
+          $scope.breed = ""
+          $scope.bio = ""
+          $scope.contact = ""
+          $scope.uploadPicture = null
+          alert("Success! Picture upload might take some time!")
+          $scope.$apply()
+        })
       })
     }
   }
